e6b42588d6612c64fc57ff46914936fb56ce1787
hamid <hr454837@gamil.com>
2023-06-12 13:37:53 +0330

    Create config

---
+++ pkg/config/app.go
@@ -0,0 +1,22 @@
+package config
+
+import(
+	"github.com/jinzhu/gorm"
+	"github.com/jinzhu/gorm/dialects/mysql"
+)
+
+var (
+	db * gorm.DB
+)
+
+func Connect(){
+	d, err := gorm.Open("mysql", "hamidr-source/09944947492h/simplerest?charsey=utf8&parseTime=True7loc=Local")
+	if err != nil{
+		panic(err)
+	}
+	db = d
+}
+
+fun GetDB() *gorm.DB{
+	return db
+}
