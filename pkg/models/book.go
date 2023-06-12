a5234faab12d5aea769a622fc930a6af66de8667
hamid <hr454837@gamil.com>
2023-06-12 15:05:41 +0330

    Create and complete models

--- pkg/models/book.go
+++ pkg/models/book.go
@@ -20,3 +20,26 @@ func init(){
 	db.AuthorMigrate(&book{})
 }
 
+func (b *Book) CreateBook() *Book{
+	db.NewRecord(b)
+	db.Craete(&b)
+	return b
+}
+
+func GetAllBooks() []Book{
+	var Books []Book
+	db.Find(&Books)
+	return Books
+}
+
+func GetBookById(Id int64) (*Book, *gorm.DB) {
+	var getBook Book 
+	db := db.Where("ID=?", Id).Find(&getBook)
+	return &getBook, db
+}
+
+func DeleteBook(ID int64) Book{
+	var book Book
+	db.Where("ID=?", ID).Delete(book)
+	return book
+}
