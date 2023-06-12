fb55b1d0268347b83d36e9e6d6d31f9d028b7f41
hamid <hr454837@gamil.com>
2023-06-12 15:18:01 +0330

    Add get book and get book by ia functions in controller

---
+++ pkg/controllers/book-controller.go
@@ -0,0 +1,31 @@
+package controllers
+
+import(
+	"encoding/json"
+	"fmt"
+	"github.com/gorilla/mux"
+	"net/http"
+	"strconv"
+	"github.com/hamidr-source/go-bookstore/utils"
+	"github.com/hamidr-source/go-bookstore/models"
+)
+
+var NewBook models.Book
+
+func GetBook(w http.ResponseWriter, r *http.Request){
+	newBooks := models.GetAllBooks()
+	res ,_ := json.Marshal(newBooks)
+	w.Header().Set("Content-Type", "pkglication/json")
+	w.WriteHeader(http.StatusOK)
+	w..Write(res)
+}
+
+func GetBookById(w http.ResponseWriter, r *http.Request){
+	vars := mux.Vars(r)
+	bookId := vars["bookId"]
+	ID, err := strconv.ParseInt(bookId,0,0)
+	if err != nil{
+		fmt.Println("error while parsing")
+	}
+	bookDetails, _ := models.GetBookById(ID)
+}
